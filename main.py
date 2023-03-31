981a6ab3a36d0ac0608fe11001636c283fa638f5
Anshu Sinha <anshujuly2@gmail.com>
2023-03-31 10:40:56 +0530

    Create a module called food.py, add a code in main.py to detect collision of the snake with the food and modified snake.py

--- main.py
+++ main.py
@@ -4,27 +4,48 @@ from turtle import Turtle, Screen
 """imported Snake class from snake module"""
 from snake import Snake
 
+"""imported Food class from the food module"""
+from food import Food
+
 """imported time module"""
 import time
 
 """created an object using the screen class"""
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor("black")
+screen.bgcolor("green")
 screen.title("Snake Game")
 screen.tracer(0)    # turning off the tracer
 
 """creating the snake object"""
 snake = Snake()
 
+"""creating the food object"""
+food = Food()
+
+"""making up the screen to listen on keystrokes"""
+screen.listen()
+
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 isGameOn = True
 
 while isGameOn:
+    """screen is updated"""
     screen.update()
     """created delay in animation"""
     time.sleep(0.1)
-
+    """snake object is called and the snake starts moving"""
     snake.moveSnake()
+
+    """if snake eats the food, the food vanishes at the 
+    given location and appears at a newer location after collision"""
+    if snake.head.distance(food) < 15:
+        food.moveFood()
+
 
 """screen will exit on click"""
 screen.exitonclick()
